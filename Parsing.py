22b7ffc4ed5307dfd338359f58b4116056d105c3
Iqbal Januadi <mjanuadi@ESRIINDONESIA.CO.ID>
2023-06-12 11:31:59 +0700

    Second Commit

--- Parsing.py
+++ Parsing.py
@@ -4,5 +4,25 @@ def notDigit(n):
     result = ''.join([i for i in n if not i.isdigit()])
     return result
 
-def take_cat(b):
-    resul_2 = 
+
+
+def ParsCat1_num(add_cat):
+    result_pars = add_cat.split("; ")[0]
+    result_pars_n = result_pars.split(" '")[0]
+    return result_pars_n
+
+def ParsCat1_cat(add_cat):
+    result_pars = add_cat.split("; ")[0]
+    result_pars_n = result_pars.split(" '")[1]
+    return result_pars_n.replace("'", " ")
+
+
+def ParsCat2_num(add_cat):
+    result_pars = add_cat.split("; ")[1]
+    result_pars_n = result_pars.split(" '")[0]
+    return result_pars_n
+
+def ParsCat2_cat(add_cat):
+    result_pars = add_cat.split("; ")[1]
+    result_pars_n = result_pars.split(" '")[1]
+    return result_pars_n.replace("'", " ")
