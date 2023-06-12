03cbcaf5ef6eafe32ab58284c376b0b0acd9fc75
Iqbal Januadi <mjanuadi@ESRIINDONESIA.CO.ID>
2023-06-12 10:45:37 +0700

    First Commit

---
+++ Parsing.py
@@ -0,0 +1,8 @@
+# Script in ArcGIS
+
+def notDigit(n):
+    result = ''.join([i for i in n if not i.isdigit()])
+    return result
+
+def take_cat(b):
+    resul_2 = 
---
+++ README.md
@@ -0,0 +1,1 @@
+# Repo for Script Processing ArcGIS Project
